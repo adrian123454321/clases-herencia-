5a8ce8f6b42fa4cf582dca9fd57ed4d03b94f2f8
adrian123454321 <adrian.lora12345@gmail.com>
2016-08-08 20:38:45 -0600

    figuras

---
+++ cuadrado.py
@@ -0,0 +1,15 @@
+from figura import Figurageometrica
+
+class cuadrado(Figurageometrica):
+
+	def __init__(self,lado):
+		super(). __init__(lado,lado):
+
+
+	def imprimir(self):
+		return= ""
+
+		for i in range (self,altura):
+			resultado += "*" *(self,base) + "\n"
+
+		return resultado
---
+++ triangulo.py
@@ -0,0 +1,6 @@
+from figura import Figurageometrica
+
+class Triangulo(Figurageometrica):
+
+	def __init__(self,base,altura):
+		super(). __init__(base,altura)
