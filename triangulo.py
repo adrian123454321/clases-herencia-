052c22d8d41b1f94c36ce14690ae37e8afc782cf
adrian123454321 <adrian.lora12345@gmail.com>
2016-08-10 20:35:26 -0600

    herencia

--- triangulo.py
+++ triangulo.py
@@ -4,3 +4,14 @@ class Triangulo(Figurageometrica):
 
 	def __init__(self,base,altura):
 		super(). __init__(base,altura)
+
+	def imprimir(self):
+		resultado =""
+
+		for i in range(self.altura)
+			resultado=" *" +(i+1) + "\n"
+
+		return resultado
+
+	def calcular_area(self):
+		return super().calcular_area()/2.0
--- uso.py
+++ uso.py
@@ -1,7 +1,7 @@
 from cuadrado import Cuadrado
 
 
-mi_cuadrado_2 = Cuadrado(10)
+
 
 
 print("1)elejir una figura")
@@ -21,9 +21,7 @@ if resp==1:
 
 
 
+
 print("Area:", mi_cuadrado_1.calcular_area())
 print(mi_cuadrado_1.imprimir())
-
-print("Area:", mi_cuadrado_2.calcular_area())
-print(mi_cuadrado_2.imprimir())
 
